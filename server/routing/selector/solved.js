1baa90be6c8a470c90ee99b6c1b5e4cb69835ff7
Julia Hay <jfh2152@columbia.edu>
2024-11-14 17:50:43 -0500

    Adding basic outline html and css for web pages. updated database organization. outline of solved.js backend added

--- server/routing/selector/solved.js
+++ server/routing/selector/solved.js
@@ -3,7 +3,24 @@ const data = db.getData("/")
 
 module.exports = (req, res) => {
     //putting solved problems in database
-    console.log(data)
+
+    //req params returns the problem number if solved
+    //if solved = -1, not solved
+    let solved = req.params.solved
+
+    if (solved == -1) {
+        //do nothing
+
+    } else if (solved >= 1 && solved <= 306) {
+        //add to "mate-in-one"
+        
+    } else if (solved >= 307 && solved <= 3718) {
+        //add to "mate-in-two"
+        
+    } else if (solved >= 3719 && solved <= 4462) {
+        //add to "mate-in-three"
+
+    }
     
     return;
 }
